5c3c14719985bf4feeccee26033ed79528a99e70
Zack Elliott <hubridnoxx@gmail.com>
2016-03-28 10:10:33 -0400

    working on website

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 /*
  * Module dependencies
  */
-var express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
+var express = require('express'),
+  stylus = require('stylus'),
+  nib = require('nib');
 
 
-var app = express()
+var app = express();
 
 function compile(str, path) {
   return stylus(str)
@@ -14,44 +14,20 @@ function compile(str, path) {
     .use(nib());
 }
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jade')
-app.use(express.logger('dev'))
-app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compile
-  }
-))
-app.use(express.static(__dirname + '/public'))
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(stylus.middleware({
+  src: __dirname + '/public',
+  compile: compile
+}));
 
-app.get('/today/interviewer', function (req, res) {
-  res.render('today_interviewer',
-{ title : 'Today' }
-)
-})
-
-app.get('/today/hr', function (req, res) {
-  res.render('today_hr',
-{ title : 'Today' }
-)
-})
-
-app.get('/candidate', function (req, res) {
-  res.render('candidate',
-{ title : 'Candidate View' }
-)
-})
-
-app.get('/candidates', function (req, res) {
-  res.render('candidates',
-{ title : 'All Candidates View' }
-)
-})
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.render('process',
-{ title : 'Recruiting Overview' }
-  )
-})
+  res.render('splash', {
+    title : 'Meet Gaptrap'
+  });
+});
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
